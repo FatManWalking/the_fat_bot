824a5244b53b4b7ee89c921f7452af2a60881d3b
FatManWalking <b.s.bulkens@gmail.com>
2021-06-13 18:45:12 +0200

    added symbolic link to lib

--- source/reinforcement_classes.py
+++ source/reinforcement_classes.py
@@ -1,3 +1,3 @@
 #import of different classes and modules for reinforcement learning from used packages tianshou amd ViZDoom frameworks
 
-from lib.tianshou.tianshou.policy.imitation import ImitationPolicy
+from lib import tianshou
