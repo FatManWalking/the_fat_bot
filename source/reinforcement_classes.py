19ac4fbd4d8ba90c35548bce3bd2b8ee384a22de
FatManWalking <b.s.bulkens@gmail.com>
2021-06-13 18:25:03 +0200

    added inits

---
+++ source/reinforcement_classes.py
@@ -0,0 +1,3 @@
+#import of different classes and modules for reinforcement learning from used packages tianshou amd ViZDoom frameworks
+
+from lib.tianshou.tianshou.policy.imitation import ImitationPolicy
