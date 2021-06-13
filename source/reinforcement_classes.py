a887be7042314eb9471d5d1c95a924f2c141d15d
FatManWalking <b.s.bulkens@gmail.com>
2021-06-13 19:11:28 +0200

    setup base class

--- source/reinforcement_classes.py
+++ source/reinforcement_classes.py
@@ -1,3 +1,25 @@
-#import of different classes and modules for reinforcement learning from used packages tianshou amd ViZDoom frameworks
+import torch
+import numpy as np
+import torch.nn.functional as F
+from typing import Any, Dict, Union, Optional
 
-from lib import tianshou
+#import of different classes and modules for reinforcement learning from used packages tianshou amd ViZDoom frameworks
+import tianshou
+from tianshou.policy import imitation
+
+class vanilla_imiation(imitation.base.ImitationPolicy):
+
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        optim: torch.optim.Optimizer,
+        **kwargs: Any,
+    ) -> None:
+
+        super().__init__(model, optim, **kwargs)
+        #self.model = model
+        #self.optim = optim
+        assert self.action_type in ["continuous", "discrete"], \
+            "Please specify action_space."
+
+
