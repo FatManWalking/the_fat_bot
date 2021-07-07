f576c48ebc9a25c9c4f857556034f973d4f81229
FatManWalking <b.s.bulkens@gmail.com>
2021-07-08 00:37:31 +0200

    hallo

--- source/main.py
+++ source/main.py
@@ -1,12 +1,155 @@
-# A simple main file, which will manage the different other files
-import gym
-import universe  # register the universe environments
+"""
+This is our version of a PPO trained doom agent.
+This main file contains a argparse for all important hyperparameters to be tuned
+including the .cfg and .wad of the example to be loaded.
 
-env = gym.make('flashgames.DuskDrive-v0')
-env.configure(remotes=1)  # automatically creates a local docker container
-observation_n = env.reset()
+This structur and the PPO agent are based on the Implementation of PPO by the OpenAI team.
+Reference:
+    "Proximal Policy Optimization Algorithms" Schulman et al.
 
-while True:
-    action_n = [[('KeyEvent', 'ArrowUp', True)] for ob in observation_n]  # your agent here
-    observation_n, reward_n, done_n, info = env.step(action_n)
-    env.render()
+And the Github https://github.com/amanda-lambda/drl-experiments/blob/master/ppo.py
+--> A PPO implementation for Flappy Bird
+"""
+
+import argparse
+from ppo import PPOAgent 
+
+#TODO: Add cfg and wad paths to load
+# Genric Options
+parser.add_argument("--mode",
+                    type=str,
+                    help="run the network in train or evaluation mode",
+                    default="train",
+                    choices=["train", "eval"])
+
+# DIRECTORY options
+parser.add_argument("--exp_name",
+                    type=str,
+                    help="name of experiment, to be used as save_dir",
+                    default="exp_model")
+parser.add_argument("--weights_dir",
+                    type=str,
+                    help="name of model to load",
+                    default="")
+
+# TRAIN options
+parser.add_argument("--n_train_iterations",
+                    type=int,
+                    help="number of iterations to train network",
+                    default=10) 
+parser.add_argument("--learning_rate",
+                    type=float,
+                    help="learning rate",
+                    default=1e-5) # PPO 1e-5
+parser.add_argument("--replay_memory_size",
+                    type=int,
+                    help="number of states to keep in memory for batching",
+                    default=10_000)
+parser.add_argument("--discount_factor",
+                    type=float,
+                    help="discount factor used for discounting return",
+                    default=0.99)
+
+# A2C/PPO specific parameters
+parser.add_argument("--n_workers",
+                    type=int,
+                    help="number of actor critic workers",
+                    default=5)
+parser.add_argument("--buffer_update_freq",
+                    type=int,
+                    help="refresh buffer after every x actions",
+                    default=20)
+parser.add_argument("--entropy_coeff",
+                    type=float,
+                    help="entropy regularization weight",
+                    default=0.01)
+parser.add_argument("--value_loss_coeff",
+                    type=float,
+                    help="value loss regularization weight",
+                    default=0.5)
+parser.add_argument("--max_grad_norm",
+                    type=int,
+                    help="norm bound for clipping gradients",
+                    default=40)
+parser.add_argument("--grad_clip",
+                    type=float,
+                    help="magnitude bound for clipping gradients",
+                    default=0.1)
+
+# LOGGING options
+parser.add_argument("--log_frequency",
+                    type=int,
+                    help="number of batches between each tensorboard log",
+                    default=100)
+parser.add_argument("--save_frequency",
+                    type=int,
+                    help="number of batches between each model save",
+                    default=1_000)
+
+
+def create_simple_game():
+    """
+    creates a simple run of the game to train the agent(s)
+
+    Hides the window and sets the color to a normalized grayscale to save resources
+    This mode is really only used for training and is tuned to save resources elsewhere
+    """
+    print("Initializing doom...")
+    game = vzd.DoomGame()
+    game.load_config(config_file_path)
+    game.set_window_visible(False)
+    game.set_mode(Mode.PLAYER)
+    game.set_screen_format(vzd.ScreenFormat.GRAY8)
+    game.set_screen_resolution(vzd.ScreenResolution.RES_640X480)
+    game.init()
+    print("Doom initialized.")
+
+    return game
+
+
+if __name__ == '__main__':
+    """
+    Hier the gives args are laoded and the model is intialized accordingly
+    """
+    options = parser.parse_args()
+
+    # Initialize a game for each worker and actions
+    games = [create_simple_game() for i in range options.n_workers]
+    n = game.get_available_buttons_size()
+    actions = [list(a) for a in it.product([0, 1], repeat=n)]
+    print(actions)
+
+    #TODO: Check where the gameinstances are needed
+    # Initialize our agent with the set parameters
+    agent = PPOAgent(options, len(actions))
+
+    # Run the training for the set number of epochs
+    if options.mode == 'train':
+        #TODO: run has been moved to ppo file. Rewrite function accordingly
+        agent, game = run(game, agent, actions, num_epochs=train_epochs, frame_repeat=frame_repeat,
+                          steps_per_epoch=learning_steps_per_epoch)
+
+        print("======================================")
+        print("Training finished. It's time to watch!")
+
+    # Reinitialize the game with window visible
+    game.close()
+    game.set_window_visible(True)
+    game.set_mode(Mode.ASYNC_PLAYER)
+    game.init()
+
+    for _ in range(episodes_to_watch):
+        game.new_episode()
+        while not game.is_episode_finished():
+            state = preprocess(game.get_state().screen_buffer)
+            best_action_index = agent.get_action(state)
+
+            # Instead of make_action(a, frame_repeat) in order to make the animation smooth
+            game.set_action(actions[best_action_index])
+            for _ in range(frame_repeat):
+                game.advance_action()
+
+        # Sleep between episodes
+        sleep(1.0)
+        score = game.get_total_reward()
+        print("Total score: ", score)
---
+++ source/ppo.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# E. Culurciello, L. Mueller, Z. Boztoprak
+# December 2020
+
+from __future__ import print_function
+import vizdoom as vzd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import random
+import itertools as it
+import skimage.transform
+
+from vizdoom import Mode
+from time import sleep, time
+from collections import deque
+from tqdm import trange
+
+# Other parameters
+frame_repeat = 12
+resolution = (30, 45)
+
+# Uses GPU if available
+if torch.cuda.is_available():
+    DEVICE = torch.device('cuda')
+    torch.backends.cudnn.benchmark = True
+else:
+    DEVICE = torch.device('cpu')
+
+
+def preprocess(img):
+    """Down samples image to resolution"""
+    img = skimage.transform.resize(img, resolution)
+    img = img.astype(np.float32)
+    img = np.expand_dims(img, axis=0)
+    return img
+
+
+def create_simple_game():
+    print("Initializing doom...")
+    game = vzd.DoomGame()
+    game.load_config(config_file_path)
+    game.set_window_visible(False)
+    game.set_mode(Mode.PLAYER)
+    game.set_screen_format(vzd.ScreenFormat.GRAY8)
+    game.set_screen_resolution(vzd.ScreenResolution.RES_640X480)
+    game.init()
+    print("Doom initialized.")
+
+    return game
+
+
+def test(game, agent):
+    """Runs a test_episodes_per_epoch episodes and prints the result"""
+    print("\nTesting...")
+    test_scores = []
+    for test_episode in trange(test_episodes_per_epoch, leave=False):
+        game.new_episode()
+        while not game.is_episode_finished():
+            state = preprocess(game.get_state().screen_buffer)
+            best_action_index = agent.get_action(state)
+
+            game.make_action(actions[best_action_index], frame_repeat)
+        r = game.get_total_reward()
+        test_scores.append(r)
+
+    test_scores = np.array(test_scores)
+    print("Results: mean: %.1f +/- %.1f," % (
+        test_scores.mean(), test_scores.std()), "min: %.1f" % test_scores.min(),
+          "max: %.1f" % test_scores.max())
+
+
+def run(game, agent, actions, num_epochs, frame_repeat, steps_per_epoch=2000):
+    """
+    Run num epochs of training episodes.
+    Skip frame_repeat number of frames after each action.
+    """
+
+    start_time = time()
+
+    for epoch in range(num_epochs):
+        game.new_episode()
+        train_scores = []
+        global_step = 0
+        print("\nEpoch #" + str(epoch + 1))
+
+        for _ in trange(steps_per_epoch, leave=False):
+            state = preprocess(game.get_state().screen_buffer)
+            action = agent.get_action(state)
+            reward = game.make_action(actions[action], frame_repeat)
+            done = game.is_episode_finished()
+
+            if not done:
+                next_state = preprocess(game.get_state().screen_buffer)
+            else:
+                next_state = np.zeros((1, 30, 45)).astype(np.float32)
+
+            agent.append_memory(state, action, reward, next_state, done)
+
+            if global_step > agent.batch_size:
+                agent.train()
+
+            if done:
+                train_scores.append(game.get_total_reward())
+                game.new_episode()
+
+            global_step += 1
+
+        agent.update_target_net()
+        train_scores = np.array(train_scores)
+
+        print("Results: mean: %.1f +/- %.1f," % (train_scores.mean(), train_scores.std()),
+              "min: %.1f," % train_scores.min(), "max: %.1f," % train_scores.max())
+
+        test(game, agent)
+        if save_model:
+            print("Saving the network weights to:", model_savefile)
+            torch.save(agent.q_net, model_savefile)
+        print("Total elapsed time: %.2f minutes" % ((time() - start_time) / 60.0))
+
+    game.close()
+    return agent, game
+
+class ActorCritic(nn.Module):
+    
+    def __init__(self, available_actions_count):
+        super(ActorCritic, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3, stride=2, bias=False),
+            nn.BatchNorm2d(8),
+            nn.ReLU()
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(8, 8, kernel_size=3, stride=2, bias=False),
+            nn.BatchNorm2d(8),
+            nn.ReLU()
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(8, 8, kernel_size=3, stride=1, bias=False),
+            nn.BatchNorm2d(8),
+            nn.ReLU()
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(8, 16, kernel_size=3, stride=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU()
+        )
+
+        #critic
+        self.critic = nn.Sequential(
+            nn.Linear(96, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
+
+        #actor
+        self.actor = nn.Sequential(
+            nn.Linear(96, 64),
+            nn.ReLU(),
+            nn.Linear(64, available_actions_count)
+        )
+
+        self.softmax = n.Softmax()
+
+        self.logsoftmax = nn.LogSoftmax()
+
+    def forward(self, x):
+        """
+        forward pass through the net producing the state value and the action logits
+
+        input: 
+            x (tensor): batch of input states to be processed
+
+        return:
+            state_value (type) = [descripton]
+            action_logits (type) = [descripton]
+        """
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        x1 = x[:, :96]  # input for the net to calculate the state value
+        x2 = x[:, 96:]  # relative advantage of actions in the state
+        state_value = self.critic(x1).reshape(-1, 1)
+        action_logits = self.actor(x2)
+        
+        return state_value, action_logits
+    
+    def act(self, x):
+        """
+        does things
+
+        Input:
+            x
+        """
+        pass
+
+    def evaluate_action(self, x, action):
+        """
+        does other things
+        """
+        pass
+
+class PPOAgent:
+    # This is nicely fitted to PPO and argparse now
+    def __init__(self, options, action_size):
+        
+        self.opt = options
+        self.action_size = action_size
+        self.memory = deque(maxlen=opt.replay_memory_size)
+        self.criterion = nn.MSELoss()
+
+        self.ppo = ActorCritic(action_size).to(DEVICE)
+
+        if self.opt.weights_dir != '':
+            print("Loading model from: ", self.opt.weights_dir)
+            self.ppo.load_state_dict(torch.load(self.opt.weights_dir))
+            
+        self.opt = optim.Adam(self.ppo.parameters(), lr=self.opt.learning_rate)
+
+    #TODO: Unchanged starting here
+    def get_action(self, state):
+        if np.random.uniform() < self.epsilon:
+            return random.choice(range(self.action_size))
+        else:
+            state = np.expand_dims(state, axis=0)
+            state = torch.from_numpy(state).float().to(DEVICE)
+            action = torch.argmax(self.q_net(state)).item()
+            return action
+
+    def update_target_net(self):
+        self.target_net.load_state_dict(self.q_net.state_dict())
+
+    def append_memory(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def train(self):
+        batch = random.sample(self.memory, self.batch_size)
+        batch = np.array(batch, dtype=object)
+
+        states = np.stack(batch[:, 0]).astype(float)
+        """
+        with open('Batch_Normal.txt', 'w') as f:
+            f.write(str(batch[:, 3]))
+        print(batch[:, 3].shape)
+
+        raise Exception('')
+        """
+        actions = batch[:, 1].astype(int)
+        rewards = batch[:, 2].astype(float)
+        next_states = np.stack(batch[:, 3]).astype(float)
+        dones = batch[:, 4].astype(bool)
+        not_dones = ~dones
+
+        row_idx = np.arange(self.batch_size)  # used for indexing the batch
+
+        # value of the next states with double q learning
+        # see https://arxiv.org/abs/1509.06461 for more information on double q learning
+        with torch.no_grad():
+            next_states = torch.from_numpy(next_states).float().to(DEVICE)
+            idx = row_idx, np.argmax(self.q_net(next_states).cpu().data.numpy(), 1)
+            next_state_values = self.target_net(next_states).cpu().data.numpy()[idx]
+            next_state_values = next_state_values[not_dones]
+
+        # this defines y = r + discount * max_a q(s', a)
+        q_targets = rewards.copy()
+        q_targets[not_dones] += self.discount * next_state_values
+        q_targets = torch.from_numpy(q_targets).float().to(DEVICE)
+
+        # this selects only the q values of the actions taken
+        idx = row_idx, actions
+        states = torch.from_numpy(states).float().to(DEVICE)
+        action_values = self.q_net(states)[idx].float().to(DEVICE)
+
+        self.opt.zero_grad()
+        td_error = self.criterion(q_targets, action_values)
+        td_error.backward()
+        self.opt.step()
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+        else:
+            self.epsilon = self.epsilon_min
+
